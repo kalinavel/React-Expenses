4315dce6fa43e4bd515aed4775f6625eadb131ab
kalinavel <kalinavelichkova@gmail.com>
2022-10-09 09:26:33 +0300

    adding state and passing data between components

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,31 +1,42 @@
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react";
 
 function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={props.data[0].title}
-        amount={props.data[0].amount}
-        date={props.data[0].date}
-      />
-      <ExpenseItem
-        title={props.data[1].title}
-        amount={props.data[1].amount}
-        date={props.data[1].date}
-      />
-      <ExpenseItem
-        title={props.data[2].title}
-        amount={props.data[2].amount}
-        date={props.data[2].date}
-      />
-      <ExpenseItem
-        title={props.data[3].title}
-        amount={props.data[3].amount}
-        date={props.data[3].date}
-      />
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+        <ExpenseItem
+          title={props.data[0].title}
+          amount={props.data[0].amount}
+          date={props.data[0].date}
+        />
+        <ExpenseItem
+          title={props.data[1].title}
+          amount={props.data[1].amount}
+          date={props.data[1].date}
+        />
+        <ExpenseItem
+          title={props.data[2].title}
+          amount={props.data[2].amount}
+          date={props.data[2].date}
+        />
+        <ExpenseItem
+          title={props.data[3].title}
+          amount={props.data[3].amount}
+          date={props.data[3].date}
+        />
+      </Card>
+    </div>
   );
 }
 
---
+++ src/components/Expenses/ExpensesFilter.js
@@ -0,0 +1,32 @@
+import "./ExpensesFilter.css";
+
+function ExpensesFilter(props) {
+
+  const filterChangeHandler = (e) => {
+    props.onChangeFilter(e.target.value);
+  };
+
+  return (
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
+        <label>Filter by year</label>
+        <select value={props.selected} onChange={filterChangeHandler}>
+          <option value="2022">
+            2022
+          </option>
+          <option value="2021">
+            2021
+          </option>
+          <option value="2020">
+            2020
+          </option>
+          <option value="2019">
+            2019
+          </option>
+        </select>
+      </div>
+    </div>
+  );
+}
+
+export default ExpensesFilter;
